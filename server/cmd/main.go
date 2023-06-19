0d4e363d68d84b2f9a6f1874106051c83e12fbec
Roger Waldron <roger.waldron@gmail.com>
2023-06-20 00:50:12 +0200

    Feat: connection to Postgres

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,12 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/RogerWaldron/go-rest-api/server/db"
+	"github.com/joho/godotenv"
+)
+
 func Run() error {
-	return nil
+	fmt.Println("starting app...")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	db, err := db.NewDatabase()
+	if err != nil { 
+		return err
+	} 
+
+	err = db.Ping(context.Background())
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("database connected oks")
+	
+	return nil 
 }
 func main(){
 	err := Run()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(err) 
 	}
 }
