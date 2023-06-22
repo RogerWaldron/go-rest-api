fa8dde33cf40fcfc0b203c03c6cc00e4a89ec6ba
Roger Waldron <roger.waldron@gmail.com>
2023-06-22 13:55:09 +0200

    Refactor: created fn for db connection str construction

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -6,17 +6,19 @@ import (
 	"os"
 
 	"github.com/RogerWaldron/go-rest-api/server/db"
+	"github.com/RogerWaldron/go-rest-api/server/internal/comment"
+	"github.com/RogerWaldron/go-rest-api/server/internal/webHTTP"
 	"github.com/joho/godotenv"
 	"github.com/rs/zerolog/log"
 )
 
-func Run() error {
-	fmt.Println("starting app...")
+func buildConnectionString() (string, error) {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal().Err(err).Msg(".env failed to load file")
-		return err
+		return "", err
 	}
+
 	connectionString := fmt.Sprintf(
 		"host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
 		os.Getenv("DB_HOST"),
@@ -26,20 +28,39 @@ func Run() error {
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("SSL_MODE"),
 	)
-	store, err := db.NewDatabase(connectionString)
+
+	return connectionString, nil
+}
+
+func Run() error {
+	fmt.Println("starting app...")
+  connect, err := buildConnectionString()
+	if err != nil {
+		panic("failed to build database connection string")
+	}
+
+	db, err := db.NewDatabase(connect)
 	if err != nil { 
 		log.Fatal().Err(err).Msg("")
 		return err
 	} 
 
-	err = store.Ping(context.Background())
+	err = db.Ping(context.Background())
 	if err != nil {
 		return err
 	}
 
-	err = store.MigrateDB()
+	err = db.MigrateDB()
 	if err != nil {
 		log.Fatal().Err(err).Msg("migration failed to setup database")
+		return err
+	}
+
+	cmtService := comment.NewService(db)
+
+	httpHandler := webHTTP.NewHandler(cmtService)
+	err = httpHandler.Serve()
+	if err != nil {
 		return err
 	}
 
